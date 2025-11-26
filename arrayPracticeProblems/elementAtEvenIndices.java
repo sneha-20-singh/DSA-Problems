adb45d9249ef32ae02c82ecc5568c02ec34cd2c2
Sneha Singh <snehasingh50246@gmail.com>
2025-11-26 22:16:49 +0530

    new files added

---
+++ arrayPracticeProblems/elementAtEvenIndices.java
@@ -0,0 +1,13 @@
+package arrayPracticeProblems;
+public class elementAtEvenIndices {
+    public static void main(String[] args) {
+        int arr[]={1,3,6,7,3,9,6,3,4};
+        for(int i=1;i<=arr.length;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(arr[i]+" ");
+            }
+        }
+    }
+}
