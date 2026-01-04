59c9aa119aa635f4c4b11b4b8e1314215bf76cdf
Sneha Singh <snehasingh50246@gmail.com>
2026-01-04 22:34:24 +0530

    Left Rotate Array By One Place

---
+++ arrayPracticeProblems/leftRotateArrByOnePlace.java
@@ -0,0 +1,28 @@
+package arrayPracticeProblems;
+import java.util.*;
+public class leftRotateArrByOnePlace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+         int []arr=new int[n];
+        System.out.println("Enter the elements of array: ");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+int temp = arr[0];
+for(int i=1;i<n;i++)
+{
+    arr[i-1]=arr[i];
+}
+arr[n-1]=temp;
+System.out.println("Array after left rotating the array by 1's places");
+for(int i=0;i<n;i++)
+{
+    System.out.print(arr[i]+" ");
+}
+sc.close();
+    }
+}
